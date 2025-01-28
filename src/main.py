258956bc261cf228214ec549c1bd6e34d30ab143
Jos Elkink <jelkink@gmail.com>
2025-01-28 12:02:10 +0000

    Add main file with argument parsing

---
+++ src/main.py
@@ -0,0 +1,33 @@
+from converter import Converter
+import argparse
+
+def main():
+
+  parser = argparse.ArgumentParser(description="Convert LaTeX to Word via Markdown.")
+
+  parser.add_argument("filename", help="Input LaTeX file")
+  parser.add_argument("-w", "--write-word", action="store_true", help="Write output to Word")
+  parser.add_argument("-b", "--bibliography", metavar="BIB_FILE", help="BibTeX file for citations")
+
+  args = parser.parse_args()
+
+  write_word = args.write_word
+  bibliography_file = args.bibliography
+  filename = args.filename
+
+  with open(filename + ".tex", "r") as file:
+    latex_content = file.read()
+
+  converter = Converter(latex_content, bibliography_file)
+
+  converter.process()
+
+  with open(filename + ".md", "w") as file:
+    file.write(converter.markdown_content())
+
+  if write_word:
+    with open(filename + ".docx", "wb") as docx_file:
+      docx_file.write(converter.word_content())
+
+if __name__ == "__main__":
+  main()
